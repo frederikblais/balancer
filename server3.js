bf8e5e3593de10ffb802e2acf084575b5db852d0
moutell1 <mohamed.outellou@yp.ca>
2021-02-16 22:25:47 -0500

    Initial commit

---
+++ server3.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var result = setTimeout(factorial(parseInt(q.n)), 3000); 
+  res.write("Result : " + result + "<br> Server 3")
+  res.end();
+}).listen(3333);
+
+function factorial(n){
+    var rval=1;
+    for (var i = 2; i <= n; i++)
+        rval = rval * i;
+    return rval;
+}
