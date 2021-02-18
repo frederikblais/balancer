829253cbd6505323c6853f4e92594ccf30ca6807
frederikblais <frederik.blais22@gmail.com>
2021-02-18 00:27:19 -0500

    bug fixes

--- app/balancer.js
+++ app/balancer.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require("url");
-var request = 0
-var line = '--------------------------------'
+var request = 0;
+var line = '--------------------------------';
 
 const servers = [
   'http://localhost:1111',
@@ -10,7 +10,7 @@ const servers = [
 ];
 
 http.createServer(function (req, res) {
-
+  var server_num = 0;
   res.writeHead(200, { "Content-Type": "text/html" });
   var p = url.parse(req.url, true).pathname;
 
@@ -20,32 +20,27 @@ http.createServer(function (req, res) {
 
     // At request #1 => send user to server 1111
     if (request == 1) {
+      var server_num = 1;
       req_port(servers[0]);
-      console.log(request)
     }
 
     // At request #2 => send user to server 2222
     else if (request == 2) {
       // redirrect to 2222
+      var server_num = 2;
       req_port(servers[1]);
-      console.log(request)
     }
       
     // At request #3 => send user to server 3333
     else if (request == 3) {
       // redirrect to 3333
+      var server_num = 3;
+      request = 0;
       req_port(servers[2]);
-      console.log(request)
-    }
-      
-    // At request #4 => send user to server 1111 and reset request count to 1
-    else {
-      request = 1;
-      req_port(servers[0]);
-      console.log(request)
     }
 
     function req_port(address) {
+      console.log("req port: "+address,server_num);
       var callingOtherServerRequest = http.request(address, function(response) {
         var str = '';
         //another chunk of data has been received, so append it to `str`
@@ -56,51 +51,29 @@ http.createServer(function (req, res) {
         //the whole response has been received, so we just print it out here
         response.on('end', function () {
           console.log(str);
+          server_num += 1;
         });
 
       }).end();
 
       callingOtherServerRequest.on('error', function (e) {
-        console.info("server "+address+ " is not responding, forwarding you to the next server.");
-        redirrect(address);
+        console.info("server " + address + " is not responding, forwarding you to the next server.");
+        server_num += 1;
+        redirrect(server_num);
       });
     }
 
-    function redirrect(address) {
-      // redirrect to 2222
-      var callingOtherServerRequest = http.request(address, function (response) {
-        console.log(request);
-        
-        if (request == 0 || request == 1 || request == 2) {
-            var str = '';
-            //another chunk of data has been received, so append it to `str`
-            response.on('data', function (chunk) {
-            str += chunk;
-          });
-  
-            //the whole response has been received, so we just print it out here
-            response.on('end', function () {
-            console.log(str);
-          });
-        }
-        else if (request == 3){
-          var str = '';
-            //another chunk of data has been received, so append it to `str`
-            response.on('data', function (chunk) {
-              str += chunk;
-          });
-  
-            //the whole response has been received, so we just print it out here
-            response.on('end', function () {
-            console.log(str);
-          });
-        }
-  
-      }).end();
-  
-      callingOtherServerRequest.on('error', function (e) {
-        console.info("uh oh...");
-      });
+    function redirrect(sn) {
+      // redirrect to next responding server
+      request += 1;
+      console.log('before if :'+ sn);
+      if (sn == 4) {
+        sn = 0;
+        req_port(servers[0]);
+      }
+      else {
+        req_port(servers[sn]);
+      }
     }
   }
   // Error handling for unknown paths
